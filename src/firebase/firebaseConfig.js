5448455db39eb6e537d8bc9a8668effbc931b263
Justin Chen <justin99chen@gmail.com>
2020-09-13 23:41:25 -0700

    updated css

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,13 +1,13 @@
-var firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSENGER_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
-  };
-// import firebaseConfigSecret from "./firebaseConfigSecret";
-// const firebaseConfig = firebaseConfigSecret;
+// var firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_DATABASE_URL,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSENGER_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+//     measurementId: process.env.REACT_APP_MEASUREMENT_ID
+//   };
+import firebaseConfigSecret from "./firebaseConfigSecret";
+const firebaseConfig = firebaseConfigSecret;
 export default firebaseConfig;
