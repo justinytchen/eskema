78fa9741034e24ac1b8325d2153b2a75050db745
Justin Chen <justin99chen@gmail.com>
2020-09-13 18:31:24 -0700

    small ui changes

--- src/components/LandingPage/demoBoards.js
+++ src/components/LandingPage/demoBoards.js
@@ -3,7 +3,7 @@ import { WidgetType } from '../../util/WidgetType';
 
 const demoBoards = {
     "demo1": {
-        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum ipsum sed purus dictum, eu aliquam est pulvinar. Cras interdum in velit vel bibendum. Nam et mi sollicitudin purus feugiat elementum in vulputate ex. Curabitur arcu felis, malesuada at magna sed, cursus dictum enim. Suspendisse id ipsum lectus. Nam suscipit fringilla ullamcorper. Duis eget tellus nec diam vehicula lobortis. Phasellus vehicula leo in mi lacinia ornare. Aenean efficitur nisi vitae fringilla gravida. Aenean pretium lacinia lectus, sed feugiat purus pharetra vel. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus a tincidunt nibh. In hac habitasse platea dictumst. Integer blandit sapien sed viverra viverra.",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum ipsum sed purus dictum, eu aliquam est pulvinar. Cras interdum in velit vel bibendum.",
         "widgets": [
             {
                 id: newGuid(),
@@ -44,7 +44,7 @@ const demoBoards = {
         ]
     },
     "demo2": {
-        "description":"Pellentesque at blandit urna. Vestibulum augue purus, viverra eget sapien non, convallis iaculis tellus. Phasellus vehicula nulla sed mauris convallis scelerisque. In hac habitasse platea dictumst. Pellentesque ac leo ac sem efficitur elementum. Nullam fermentum id sem eu porttitor. Cras vestibulum, ipsum ut dictum dignissim, diam enim vehicula est, vitae euismod ipsum odio eget odio. Integer interdum ligula purus, quis finibus magna finibus in. Suspendisse sed mauris tincidunt, fringilla lacus vel, dignissim ipsum. Nullam eu semper lectus. Quisque non consequat purus. Sed porttitor urna dolor, nec maximus nisi cursus eget. Fusce elit ex, dignissim at lobortis ut, posuere sit amet urna. Praesent tincidunt tincidunt quam, consequat mattis libero elementum non. Vivamus convallis sit amet risus a pellentesque.",
+        "description":"Pellentesque at blandit urna. Vestibulum augue purus, viverra eget sapien non, convallis iaculis tellus. ",
         "widgets": [
             {
                 id: newGuid(),
@@ -73,7 +73,7 @@ const demoBoards = {
         ]
     },
     "demo3": {
-        "description":"In feugiat consectetur faucibus. Suspendisse luctus urna eu velit aliquet accumsan. Cras sit amet lobortis est. Mauris eu tempor purus. Vivamus id nunc sit amet elit dapibus elementum. Praesent non erat cursus, tristique ex sed, convallis nisl. Nunc feugiat justo at leo malesuada finibus id at lectus. Curabitur sit amet ante sed ipsum ultrices porttitor.",
+        "description":"In feugiat consectetur faucibus. Suspendisse luctus urna eu velit aliquet accumsan. Cras sit amet lobortis est. Mauris eu tempor purus. ",
         "widgets": [
             {
                 id: newGuid(),
